4389d179921154cb61463569b2ff935a44a00336
Ant Weiss <anton@otomato.link>
2019-08-26 15:39:16 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -76,7 +76,7 @@ my_task = KubernetesPodOperator(
     namespace='default',
     image='alpine',
     cmds=[
-        "sleep 30 && exit 0",
+        "sleep", "30"
     ],
     name="email-test",
     image_pull_secrets="regcred",
@@ -91,7 +91,7 @@ my_other_task = KubernetesPodOperator(
     namespace='default',
     image='alpine',
     cmds=[
-        "sleep 100 && exit 0",
+        "sleep", "100"
     ],
     name="succ_email-test",
     image_pull_secrets="regcred",
